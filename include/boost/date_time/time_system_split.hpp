1948a0f5aa2afd826806e8a3c4c650ed96a529ab
Andrey Semashev <andrey.semashev@gmail.com>
2020-04-04 17:11:14 +0300

    Make public headers self-contained.
    This commit:
    
    - Adds missing includes.
    - Adjusts gregorian namespace qualification in a few places to use use
      equivalent symbols from date_time and not have to include gregorian
      headers.
    - Converts tabs to spaces and trims trailing spaces.
    
    This makes the self-contained header tests pass on Linux.

--- include/boost/date_time/time_system_split.hpp
+++ include/boost/date_time/time_system_split.hpp
@@ -2,7 +2,7 @@
 #define DATE_TIME_TIME_SYSTEM_SPLIT_HPP
 
 /* Copyright (c) 2002,2003,2005 CrystalClear Software, Inc.
- * Use, modification and distribution is subject to the 
+ * Use, modification and distribution is subject to the
  * Boost Software License, Version 1.0. (See accompanying
  * file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
  * Author: Jeff Garland, Bart Garst
@@ -11,15 +11,17 @@
 
 
 #include <string>
-#include <boost/config.hpp>
-#include "boost/date_time/compiler_config.hpp"
-#include "boost/date_time/special_defs.hpp"
+#include <boost/cstdint.hpp>
+#include <boost/date_time/compiler_config.hpp>
+#include <boost/date_time/time_defs.hpp>
+#include <boost/date_time/special_defs.hpp>
+#include <boost/date_time/wrapping_int.hpp>
 
 namespace boost {
 namespace date_time {
 
   //! An unadjusted time system implementation.
-#if (defined(BOOST_DATE_TIME_NO_MEMBER_INIT))  
+#if (defined(BOOST_DATE_TIME_NO_MEMBER_INIT))
   template<typename config, boost::int32_t ticks_per_second>
 #else
   template<typename config>
@@ -35,7 +37,7 @@ namespace date_time {
     typedef typename config::resolution_traits   resolution_traits;
 
     //86400 is number of seconds in a day...
-#if (defined(BOOST_DATE_TIME_NO_MEMBER_INIT))  
+#if (defined(BOOST_DATE_TIME_NO_MEMBER_INIT))
     typedef date_time::wrapping_int<int_type, INT64_C(86400) * ticks_per_second > wrap_int_type;
 #else
    private:
@@ -57,10 +59,10 @@ namespace date_time {
         return time_rep_type(date_type(not_a_date_time),
                              time_duration_type(not_a_date_time));
       case pos_infin:
-        return time_rep_type(date_type(pos_infin), 
+        return time_rep_type(date_type(pos_infin),
                              time_duration_type(pos_infin));
       case neg_infin:
-        return time_rep_type(date_type(neg_infin), 
+        return time_rep_type(date_type(neg_infin),
                              time_duration_type(neg_infin));
       case max_date_time: {
         time_duration_type td = time_duration_type(24,0,0,0) - time_duration_type(0,0,0,1);
@@ -72,7 +74,7 @@ namespace date_time {
       default:
         return time_rep_type(date_type(not_a_date_time),
                              time_duration_type(not_a_date_time));
-        
+
       }
 
     }
@@ -80,8 +82,8 @@ namespace date_time {
     static
     BOOST_CXX14_CONSTEXPR
     time_rep_type get_time_rep(const date_type& day,
-			       const time_duration_type& tod,
-			       date_time::dst_flags /* dst */ = not_dst)
+                               const time_duration_type& tod,
+                               date_time::dst_flags /* dst */ = not_dst)
     {
       if(day.is_special() || tod.is_special()) {
         if(day.is_not_a_date() || tod.is_not_a_date_time()) {
@@ -153,19 +155,19 @@ namespace date_time {
     }
     static BOOST_CXX14_CONSTEXPR
     time_rep_type add_days(const time_rep_type& base,
-			   const date_duration_type& dd)
+                           const date_duration_type& dd)
     {
       return time_rep_type(base.day+dd, base.time_of_day);
     }
     static BOOST_CXX14_CONSTEXPR
     time_rep_type subtract_days(const time_rep_type& base,
-				const date_duration_type& dd)
+                                const date_duration_type& dd)
     {
       return split_timedate_system::get_time_rep(base.day-dd, base.time_of_day);
     }
     static BOOST_CXX14_CONSTEXPR
     time_rep_type subtract_time_duration(const time_rep_type& base,
-					 const time_duration_type& td)
+                                         const time_duration_type& td)
     {
       if(base.day.is_special() || td.is_special())
       {
@@ -184,7 +186,7 @@ namespace date_time {
     }
     static BOOST_CXX14_CONSTEXPR
     time_rep_type add_time_duration(const time_rep_type& base,
-				    time_duration_type td)
+                                    time_duration_type td)
     {
       if(base.day.is_special() || td.is_special()) {
         return split_timedate_system::get_time_rep(base.day, td);
@@ -194,7 +196,7 @@ namespace date_time {
         return subtract_time_duration(base,td1);
       }
 
-      wrap_int_type day_offset(base.time_of_day.ticks());      
+      wrap_int_type day_offset(base.time_of_day.ticks());
       date_duration_type day_overflow(static_cast< typename date_duration_type::duration_rep_type >(day_offset.add(td.ticks())));
 
       return time_rep_type(base.day+day_overflow,
@@ -202,7 +204,7 @@ namespace date_time {
     }
     static BOOST_CXX14_CONSTEXPR
     time_duration_type subtract_times(const time_rep_type& lhs,
-				      const time_rep_type& rhs)
+                                      const time_rep_type& rhs)
     {
       date_duration_type dd = lhs.day - rhs.day;
       if (BOOST_LIKELY(!dd.is_special())) {
@@ -215,7 +217,7 @@ namespace date_time {
         return td+td2;
       }
     }
-    
+
   };
 
 } } //namespace date_time
