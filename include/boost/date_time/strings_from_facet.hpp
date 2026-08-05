5f0af8c1966b59924383a6af7d544233ee12661c
Anton Karpov <karpovantonme@gmail.com>
2026-08-05 17:27:51 +0300

    Fix three parameter tags
    local_is_dst documents dst_length_minutes; the parameter is dst_length,
    and it is a time_duration_type rather than a count of minutes.
    
    gather_month_strings and gather_weekday_strings tag charT with @param
    while it is the template parameter, so Doxygen looks for an argument by
    that name and finds none.

--- include/boost/date_time/strings_from_facet.hpp
+++ include/boost/date_time/strings_from_facet.hpp
@@ -23,7 +23,7 @@ namespace boost { namespace date_time {
  *  all the month strings from a locale.  This is handy when building
  *  custom date parsers or formatters that need to be localized.
  *
- *@param charT The type of char to use when gathering typically char
+ *@tparam charT The type of char to use when gathering typically char
  *             or wchar_t.
  *@param locale The locale to use when gathering the strings
  *@param short_strings True(default) to gather short strings,
@@ -73,7 +73,7 @@ gather_month_strings(const std::locale& locale, bool short_strings=true)
  *  'Sunday'.  This is handy when building custom date parsers or
  *  formatters that need to be localized.
  *
- *@param charT The type of char to use when gathering typically char
+ *@tparam charT The type of char to use when gathering typically char
  *             or wchar_t.
  *@param locale The locale to use when gathering the strings
  *@param short_strings True(default) to gather short strings,
